4d4d6e6b85f17e1a49c96b46dc9d731276a3556c
hullymully <94529090+HullyMully@users.noreply.github.com>
2025-03-16 19:56:31 +0600

    fix: update app layout and dependencies

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,8 @@
+import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
 import { Provider } from 'react-redux';
 import { store } from '../store';
-import '@/styles/globals.css';
 
 export default function MyApp({
   Component,
